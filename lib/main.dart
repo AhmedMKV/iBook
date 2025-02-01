437b86058f6de49ca6c70ec382b345f1af7fd6a4
AhmedMKV <aelmorsyss@gmail.com>
2025-02-01 20:09:38 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const IBooksApp());
+}
+
+class IBooksApp extends StatelessWidget {
+  const IBooksApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
+
