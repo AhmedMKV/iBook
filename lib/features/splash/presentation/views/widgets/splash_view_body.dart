262946458ebea1c6c61934c6b1745982de529d12
AhmedMKV <aelmorsyss@gmail.com>
2025-02-02 16:07:29 +0200

    add slide animation to splash view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:ibook/constants.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    slidingAnimation = Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+
+  }
+@override
+  void dispose() {
+
+    super.dispose();
+    animationController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -13,12 +38,17 @@ class SplashViewBody extends StatelessWidget {
         SizedBox(
           height: 15,
         ),
-        Text(
-          'Unlocking Minds, One Page at a Time',
-          style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontFamily: kFont),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context,_) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: Text(
+                'Unlocking Minds, One Page at a Time',
+                style: TextStyle(fontSize: 24, fontFamily: kFont),
+              ),
+            );
+          }
         )
       ],
     );
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class IBookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
       debugShowCheckedModeBanner: false,
       home: SplashView(),
     );
