a3dadf4020af0ff23c9e846b1ec93e5acd92126c
AhmedMKV <aelmorsyss@gmail.com>
2025-02-04 13:56:43 +0200

    add custom scroll view

---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:ibook/features/home/presentation/views/widgets/custom_best_seller_item.dart';
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+   physics: NeverScrollableScrollPhysics(),
+        itemCount: 7,
+        itemBuilder: (context , index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: CustomBestSellerItem(),
+          );
+        }
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ibook/core/utils/styles.dart';
+import 'package:ibook/features/home/presentation/views/widgets/best_seller_list_view.dart';
 import 'package:ibook/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:ibook/features/home/presentation/views/widgets/custom_best_seller_item.dart';
 
 import 'package:ibook/features/home/presentation/views/widgets/featured_list_view.dart';
 
@@ -10,18 +10,28 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppBar(),
-         FeaturedListView(),
-        SizedBox(height: 50,),
-        Padding(
-          padding: const EdgeInsets.all(16),
-          child: Text('Best Seller ',
-            style: Styles.text22,),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              FeaturedListView(),
+              SizedBox(height: 50,),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text('Best Seller ',
+                  style: Styles.text22,),
+              ),
+
+            ],
+          ),
         ),
-        CustomBestSellerItem()
+        SliverFillRemaining(
+          child: BestSellerListView()
+          ,
+        )
       ],
     );
   }
