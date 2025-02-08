785361434e2d4cf8892c7abdd83eade61e8a3878
AhmedMKV <aelmorsyss@gmail.com>
2025-02-08 17:08:21 +0200

    create home repo and failures file and add dartz package

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:ibook/core/errors/failures.dart';
+import 'package:ibook/features/home/data/models/book_model/BookModel.dart';
+
+abstract class HomeRepo{
+  Future<Either <Failures,List<BookModel>>> fetchFeaturedBooks();
+  Future<Either <Failures,List<BookModel>>> fetchBestSellerBooks();
+}
